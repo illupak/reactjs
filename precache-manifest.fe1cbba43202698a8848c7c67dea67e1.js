9b9f9f345719bb4c27c0f4b0b3a4cc9f9f912658
Illupak <illupak@localhost.localdomain>
2024-02-01 13:59:11 +0530

    Updates

---
+++ precache-manifest.fe1cbba43202698a8848c7c67dea67e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a34daf993b7fb81fcda91926f3975fe",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "993962c499d662bf5625",
+    "url": "/reactjs/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6699f2559d7ab8601410",
+    "url": "/reactjs/static/js/2.b49763a4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/reactjs/static/js/2.b49763a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b37b762b890e42d7df9",
+    "url": "/reactjs/static/js/3.b63c34da.chunk.js"
+  },
+  {
+    "revision": "993962c499d662bf5625",
+    "url": "/reactjs/static/js/main.beb670b5.chunk.js"
+  },
+  {
+    "revision": "881af99a9f1cd890d64f",
+    "url": "/reactjs/static/js/runtime-main.bc7cfa8d.js"
+  }
+]);
